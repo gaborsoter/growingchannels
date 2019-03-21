5c43e5e9af6198524e6cc88998c0d38b3b4dd7c8
developersforbuilt <ej17718@bristol.ac.uk>
2019-03-21 15:24:38 +0000

    integration finished

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -14,7 +14,7 @@ np.random.seed()
 # Hyperparameters
 height = 50 # height of the touchpad
 width  = 50 # width of the touchpad
-max_iterations = 100000 # maximum number of iterations
+max_iterations = 1000 # maximum number of iterations
 max_individuals = 1 # number of individuals, need to be even
 length = 10
 rulearray = ['A', 'B', '+', '-', '[', ']']
@@ -100,8 +100,8 @@ class GeneticAlgorithm:
 		fitnessarray = np.zeros(population.shape[0], dtype = 'uint8')
 
 		for k in range(population.shape[0]):
-			for i in range(7):
-				for j in range(i+1, 8):
+			for i in range(19):
+				for j in range(i+1, 20):
 					if population[k].genome[i] != population[k].genome[j] and population[k].genome[i] != (population[k].genome[j] + (j-i)) and population[k].genome[i] != (population[k].genome[j] - (j - i)):
 						fitnessarray[k] += 1		
 
@@ -113,7 +113,6 @@ class GeneticAlgorithm:
 	def l_system(self, iter_lsystem):
 
 		char_array = list(map(lambda x: rulearray[x], population[i].genome))
-		
 		sep = ''
 		rule = sep.join(char_array)
 		print "rules: ", rule
@@ -127,22 +126,7 @@ class GeneticAlgorithm:
 					new_string.append(rule[18:20])
 				else:
 					new_string.append(old_string[k])
-
 			old_string = sep.join(new_string)
-
-		## CHECK THAT POPS DONT OUTNUMBER APPENDS ##
-		pops = 0
-		appends = 0
-		for item in old_string:
-			if item == ']':
-				pops += 1
-			elif item == '[':
-				appends += 1
-			if pops > appends:
-				print(old_string)
-				print appends, pops
-				print 'ERROR: CLOSE BRACKET (POP) OUTNUMBER OPEN BRACKETS (APPEND)'
-				sys.exit()
 
 		return old_string
 
@@ -154,49 +138,49 @@ class GeneticAlgorithm:
 		turn = math.radians(25)
 		stack = []
 
-		for item in final_string:
-			if item == 'A' or item == 'B':
-				x_new = int(position[0]+length*math.cos(heading))
-				y_new = int(position[1]+length*math.sin(heading))
-				new_position = ( x_new, y_new )
-				cv2.line(img,position,new_position,255,1)
-				position = new_position
-				# print '[ FRWD ] ', position
-			elif item == '+':
-				heading = heading + turn
-				# print '[ RGHT ] ', math.degrees(turn_right)
-			elif item == '-':
-				heading = heading - turn
-				# print '[ LEFT ] ', math.degrees(turn_left)
-			elif item == '[':
-				stack.append((position, heading))
-				# print '[ APPEND ]', stack
-			elif item == ']':
-				position, heading = stack.pop() #len(stack)-1
-				# print '[ POP  ] ', position, heading
-			else:
-				print '[ NOP  ] ', codebit
-
+		try:	
+			for item in final_string:
+				if item == 'A' or item == 'B':
+					x_new = int(position[0]+length*math.cos(heading))
+					y_new = int(position[1]+length*math.sin(heading))
+					new_position = ( x_new, y_new )
+					cv2.line(img,position,new_position,255,1)
+					position = new_position
+					# print '[ FRWD ] ', position
+				elif item == '+':
+					heading = heading + turn
+					# print '[ RGHT ] ', math.degrees(turn_right)
+				elif item == '-':
+					heading = heading - turn
+					# print '[ LEFT ] ', math.degrees(turn_left)
+				elif item == '[':
+					stack.append((position, heading))
+					# print '[ APPEND ]', stack
+				elif item == ']':
+					position, heading = stack.pop() #len(stack)-1
+					# print '[ POP  ] ', position, heading
+				else:
+					print '[ NOP  ] ', codebit
+		except Exception as e:
+			print "Genome not executable"
 		cv2.imshow('Channels', img)
-		cv2.waitKey(0)
+		cv2.waitKey(5)
 
 class Individual_Lsystem():
 	def __init__(self):
 		self.fitness = 0
-		# self.genome = np.random.randint(low = 0, high = 6, size=(20))
-		self.genome = [0, 3, 4, 4, 1, 5, 2, 1, 5, 2, 0, 4, 2, 0, 1, 5, 3, 1, 0, 0] # Existing L-systems rules: A-[[B]+B]+A[+AB]-BAA
+		self.genome = np.random.randint(low = 0, high = 6, size=(20))
+		# self.genome = [0, 3, 4, 4, 1, 5, 2, 1, 5, 2, 0, 4, 2, 0, 1, 5, 3, 1, 0, 0] # Existing L-systems rules: A-[[B]+B]+A[+AB]-BAA
 		self.probability = 0
 	
 # Creating objects
 population = np.array([Individual_Lsystem() for i in range(max_individuals)])
-
 
 iteration = 0
 while iteration < max_iterations:
 	geneticAlgorithm = GeneticAlgorithm()
 	string_to_draw = geneticAlgorithm.l_system(4)
 	geneticAlgorithm.drawing(string_to_draw)
-	# geneticAlgorithm.rouletteWheel()
 	geneticAlgorithm.crossover()
 	geneticAlgorithm.mutation()
 
